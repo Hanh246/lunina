a36ecec9d41982b9ee54e590ea269682bbc8ede0
Hanh246 <149309654+Hanh246@users.noreply.github.com>
2026-04-16 11:11:42 +0700

    delete auto size

--- src/main/java/com/source/lunina/main/service/UserExcelExporter.java
+++ src/main/java/com/source/lunina/main/service/UserExcelExporter.java
@@ -43,7 +43,7 @@ public class UserExcelExporter {
     }
 
     private void createCell(Row row, int columnCount, Object value, CellStyle style) {
-        sheet.autoSizeColumn(columnCount);
+//        sheet.autoSizeColumn(columnCount);
         Cell cell = row.createCell(columnCount);
         if (value instanceof Long) {
             cell.setCellValue((Long) value);
